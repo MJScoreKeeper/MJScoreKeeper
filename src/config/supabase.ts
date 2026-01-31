d5601f1d9f99cbbfb0dc17f04e7e131c7a0b1cd9
garylchan <garylchan@gmail.com>
2026-01-31 09:58:18 -0800

    Update Supabase URL and anon key

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yhowylubaurpcgewonwe.supabase.co';
-const supabaseAnonKey = 'sb_publishable_f5OK93r34x3SrC6cZaTK6Q_OboP-xOY';
+const supabaseUrl = 'https://itqbijdwsyxeairbynes.supabase.co';
+const supabaseAnonKey = 'sb_publishable_zkR9eREVcOGK0sGSAc3g0A_x_Iyai_a';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
